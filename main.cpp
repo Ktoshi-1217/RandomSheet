4a6df9fe9708a3a6906fc8a0e428cdf4d0e9f661
Ktoshi-1217 <kondoutoshi1217@gmail.com>
2026-04-01 13:27:50 +0900

    Qtの表テスト

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <QApplication>
-#include <QLabel>
+#include <QTableWidget>
+#include <QStringList>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    QApplication app(argc, argv); // Qtの初期設定だろう、args送って何している？
 
-    QLabel label("Hello QtWorld!");
-    label.resize(200, 100);
-    label.show();
+    QTableWidget table; // Cppって型宣言時点でコンストラクタ呼ばれるんだっけ？
 
-    return app.exec();
+    table.setRowCount(3); // テーブルの定義を上でして、ここは行が3
+    table.setColumnCount(3); // ここは列が3
+
+    QStringList headers = {"名称", "分類", "詳細"}; // 型名的にQt用の文字列リスト？vectorとかとは何が違う？
+    table.setHorizontalHeaderLabels(headers); // 英語ムズイ、
+
+    table.setItem(0, 0, new QTableWidgetItem("ベホイミ")); // 恐らく1行1列目に入れる内容
+    table.setItem(0, 1, new QTableWidgetItem("呪文")); // こっちも1行2列目だろうね
+    table.setItem(0, 2, new QTableWidgetItem("回復")); // で、個人的にはcss、json、yamlのどれかで行こうとは思ってる
+
+    table.show(); // ここで表示すかね
+
+    return app.exec(); // 実行してWindows閉じたら即終了
 }
